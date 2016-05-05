c2b7acdf8c1c9fe89560e4c51c06fc3d491aecc3
kkellyin90 <keith.kelly.a@gmail.com>
2016-05-05 20:13:06 +0530

    Added files via upload

---
+++ daysInMonth.java
@@ -0,0 +1,53 @@
+
+public class daysInMonth {
+
+	public static void main(String[] args) {
+		// Write a program to print total number of days in the given month.
+		
+		int month = 01;
+		
+		switch (month) {
+		case 1:
+			System.out.println("January has 31 days");
+			//break;
+		
+		case 2:
+			System.out.println("February has 29 days");
+			//break;
+		case 3:
+			System.out.println("March has 31 days");
+			//break;
+		case 4:
+			System.out.println("April has 30 days");
+			//break;
+		case 5:
+			System.out.println("May has 31 days");
+			//break;
+		case 6:
+			System.out.println("June has 30 days");
+			//break;
+		case 7:
+			System.out.println("July has 31 days");
+			//break;
+		case 8:
+			System.out.println("August has 31 days");
+			//break;
+		case 9:
+			System.out.println("September has 30 days");
+			//break;
+		case 10:
+			System.out.println("October has 31 days");
+			//break;
+		case 11:
+			System.out.println("November has 30 days");
+			//break;
+		case 12:
+			System.out.println("December has 31 days");
+			//break;
+		}
+		
+		
+
+	}
+
+}
